739ee76e6ae981c3354a671c711fdbaeccca6ab8
Imkerix <Imkerixus@gmail.com>
2013-10-27 14:09:49 +0100

    methods added ---------------- public get(String):object public set(String property, String Value):object ---------------- methods deleted ------------------ public getName():String public setName(String):void public getFamilyname():String public setFamilyname(String):void public getEmail():String public setEmail(String):void public getTel():String public setTel(String):void public getMobile():String public setMobile(String):void public getStreet():String public setStreet(String):void public getCountry():String public setCountry(String):void public getHousenumber():int public setHousenumber(int):void public getBirthday():Date public setBirthday(Date):void

--- src/floatingObjects/Mail.java
+++ src/floatingObjects/Mail.java
@@ -1,9 +1,12 @@
 package floatingObjects;
 
 import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;				
 import java.util.Date;
 
+@SuppressWarnings("serial")
 public class Mail implements Serializable
 {
 
@@ -16,96 +19,94 @@ public class Mail implements Serializable
 	{
 		mail_id = p_mail_id;
 	}
-
-//// Begin : Subject ////
-	public String getSubject() 
-	{
-		return subject;
-	}
-
-
-	public void setSubject(String subject) 
-	{
-		this.subject = subject;
-	}
-//// End : Subject ////
-//// Begin : Content ////
-	public String getContent() 
-	{
-		return content;
-	}
-
-
-	public void setContent(String content) 
-	{
-		this.content = content;
-	}
-//// End : Content ////
-//// Begin : SenderEmail ////
-	public String getSenderEmail() 
-	{
-		return senderEmail;
-	}
-
-
-	public void setSenderEmail(String senderEmail) 
-	{
-		this.senderEmail = senderEmail;
-	}
-//// End : SenderEmail ////
-//// Begin : RecipientEmail ////	
-	public String getRecipientEmail() {
-		return recipientEmail;
-	}
-
-
-	public void setRecipientEmail(String recipientEmail) 
-	{
-		this.recipientEmail = recipientEmail;
-	}
-//// End : RecipientEmail ////
-//// Begin : cc ////
-	public String getCc() 
-	{
-		return cc;
-	}
-
-
-	public void setCc(String cc) 
-	{
-		this.cc = cc;
-	}
-//// End : cc ////
-//// Begin : bcc ////
-	public String getBcc() 
-	{
-		return bcc;
-	}
-
-
-	public void setBcc(String bcc) 
-	{
-		this.bcc = bcc;
-	}
-//// End : bcc ////
-//// Begin : Attachements ////
-	public ArrayList<String> getAttachements() 
-	{
-		return attachements;
-	}
-
-
-	public void setAttachements(ArrayList<String> attachements) 
-	{
-		this.attachements = attachements;
-	}
-//// End : Attachements ////	
 	
-	public int getMail_id()
+////Begin : Ultimative getter ////
+	public Object get(String p_ArgumentToGet)
 	{
-		return mail_id;
+		switch (p_ArgumentToGet) 
+		{
+			case "subject": return subject;	
+			case "content": return content;
+			case "senderemail": return senderEmail;
+			case "recipientemail": return recipientEmail;
+			case "cc": return cc;
+			case "bcc": return bcc;
+			case "date": return date.toString();
+			case "mailid": return Integer.toString(mail_id);
+			case "attachements": return attachements;
+		
+			default: return "Mail oder Argument fehlerhaft";	
+		}
 	}
+//// Begin : Ultimative getter ////	
 	
+////Begin : Ultimative setter ////
+	public Object set(String[] args)
+	{
+		switch (args[3]) 
+		{
+			case "subject": 
+				subject = args[4]; 
+			return args[3]+"="+args[4];	
+			
+			case "content": 
+				String temp_content = "";
+				for(int i = 4; i<args.length; i++)
+				{
+					temp_content+=args[i]+" ";
+				}
+				content = temp_content;
+			return args[3]+"="+args[4];
+			
+			case "senderemail": 
+				senderEmail = args[4];
+			return args[3]+"="+args[4];
+			
+			case "recipientemail": 
+				recipientEmail = args[4];
+			return args[3]+"="+args[4];
+			
+			case "cc": 
+				cc = args[4];
+			return args[3]+"="+args[4];
+			
+			case "bcc": 
+				bcc = args[4];
+			return args[3]+"="+args[4];
+			
+			case "date":
+				try 
+				{
+					SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
+					date = formatter.parse(args[4]);
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+			return args[3]+"="+args[4];
+			
+			case "mail_id": 
+				mail_id = Integer.parseInt(args[4]);
+			return args[3]+"="+args[4];
+			
+			case "attachements_add":
+				for (String arg : args) 
+				{
+					attachements.add(arg);
+				}
+			return args[3]+"="+"all Added";
+			
+			case "attachements_replace":
+				attachements.clear();
+				for (String arg : args) 
+				{
+					attachements.add(arg);
+				}
+			return args[3]+"="+"all Added";
+		
+			default: return "Mail oder Argument fehlerhaft";	
+		}
+	}
+//// Begin : Ultimative setter ////	
 	
 	
 }
