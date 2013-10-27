3f74cf89f103cbe708073cca59a3f422dcff8ecd
Imkerix <Imkerixus@gmail.com>
2013-10-27 14:16:47 +0100

    methods added ---------------- public get(String):object public set(String property, String Value):object ---------------- methods deleted ------------------ public getName():String public setName(String):void public getFamilyname():String public setFamilyname(String):void public getEmail():String public setEmail(String):void public getTel():String public setTel(String):void public getMobile():String public setMobile(String):void public getStreet():String public setStreet(String):void public getCountry():String public setCountry(String):void public getHousenumber():int public setHousenumber(int):void public getBirthday():Date public setBirthday(Date):void

--- src/floatingObjects/Contact.java
+++ src/floatingObjects/Contact.java
@@ -1,9 +1,11 @@
 package floatingObjects;
 
 import java.io.Serializable;
-//import java.util.ArrayList;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
+@SuppressWarnings("serial")
 public class Contact implements Serializable
 {
 
@@ -26,136 +28,81 @@ public class Contact implements Serializable
 		birthday = p_birthday;
 		contact_id = p_contact_id;
 	}
-//// Begin : name ////
-	public String getName() 
-	{
-		return name;
-	}
 
-	public void setName(String name) 
+//// Begin : Ultimative getter ////	
+	public Object get(String p_ValueToGet)
 	{
-		this.name = name;
+		switch (p_ValueToGet) 
+		{
+			case "name": return name;	
+			case "familyname": return familyname;
+			case "email": return email;
+			case "tel": return tel;
+			case "mobile": return mobile;
+			case "street": return street;
+			case "housenumber": return housenumber;
+			case "country": return country;
+			case "birthday": return birthday;
+			case "contactid": return contact_id;
+				
+			default: return "Contact oder Argument fehlerhaft";
+		}
 	}
-//// End : name ////
-//// Begin : Familyname ////
-	public String getFamilyname() 
+//// End : Ultimative getter ////
+//// Begin : Ultimative setter ////	
+	public Object set(String p_ArgumentToChange, String p_ValueToSet)
 	{
-		return familyname;
+		switch (p_ArgumentToChange) 
+		{
+			case "name": 
+				name = p_ValueToSet;
+			return p_ArgumentToChange+"="+p_ValueToSet;	
+			
+			case "familyname":
+				familyname = p_ValueToSet;
+			return p_ArgumentToChange+"="+p_ValueToSet;
+			
+			case "email": 
+				email = p_ValueToSet;
+			return p_ArgumentToChange+"="+p_ValueToSet;
+			
+			case "tel": 
+				tel = p_ValueToSet;
+			return p_ArgumentToChange+"="+p_ValueToSet;
+			
+			case "mobile":
+				mobile = p_ValueToSet;
+			return p_ArgumentToChange+"="+p_ValueToSet;
+			
+			case "street": 
+				street = p_ValueToSet;
+			return p_ArgumentToChange+"="+p_ValueToSet;
+			
+			case "housenumber":
+				housenumber = Integer.parseInt(p_ValueToSet);
+			return p_ArgumentToChange+"="+p_ValueToSet;
+			
+			case "country":
+				country = p_ValueToSet;
+			return p_ArgumentToChange+"="+p_ValueToSet;
+			
+			case "birthday":
+				try 
+				{
+					SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+					birthday = formatter.parse(p_ValueToSet);
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+			return p_ArgumentToChange+"="+p_ValueToSet;
+			
+			case "contactid":
+				contact_id = Integer.parseInt(p_ValueToSet);
+			return p_ArgumentToChange+"="+p_ValueToSet;
+				
+			default: return "Contact oder Argument fehlerhaft";
+		}
 	}
-
-	public void setFamilyname(String familyname) 
-	{
-		this.familyname = familyname;
-	}
-//// End : Familyname ////
-//// Begin : Email ////
-	public String getEmail() 
-	{
-		return email;
-	}
-
-	public void setEmail(String email) 
-	{
-		this.email = email;
-	}
-//// End : Email ////
-//// Begin : Tel ////
-	public String getTel() 
-	{
-		return tel;
-	}
-
-	public void setTel(String tel) 
-	{
-		this.tel = tel;
-	}
-//// End : Tel ////
-//// Begin : Mobile ////
-	public String getMobile() 
-	{
-		return mobile;
-	}
-
-	public void setMobile(String mobile) 
-	{
-		this.mobile = mobile;
-	}
-//// End : Mobile ////
-//// Begin : Street ////
-	public String getStreet() 
-	{
-		return street;
-	}
-
-	public void setStreet(String street) 
-	{
-		this.street = street;
-	}
-//// End : Street ////
-//// Begin : Country ////
-	public String getCountry() 
-	{
-		return country;
-	}
-
-	public void setCountry(String country) 
-	{
-		this.country = country;
-	}
-//// End : Country ////
-//// Begin : Housenumber ////
-	public int getHousenumber() 
-	{
-		return housenumber;
-	}
-
-	public void setHousenumber(int housenumber) 
-	{
-		this.housenumber = housenumber;
-	}
-//// End : Housenumber ////
-//// Begin : Birthday ////
-	public Date getBirthday() 
-	{
-		return birthday;
-	}
-
-	public void setBirthday(Date birthday) 
-	{
-		this.birthday = birthday;
-	}
-//// End : Birthday ////
-	public int getContact_id() 
-	{
-		return contact_id;
-	}
-	
-	
-//	public Object get(String p_ValueToGet){
-//		switch (p_ValueToGet) {
-//		case "name": return name;	
-//		case "familyname": return familyname;
-//		case "email": return email;
-//		case "tel": return tel;
-//		case "mobile": return mobile;
-//		case "street": return street;
-//		case "housenumber": return housenumber;
-//		case "country": return country;
-//		case "birthday": return birthday;
-//			
-//		default: ArrayList<Object> contactData = new ArrayList<Object>();
-//				contactData.add(name);
-//				contactData.add(familyname);
-//				contactData.add(email);
-//				contactData.add(tel);
-//				contactData.add(mobile);
-//				contactData.add(street);
-//				contactData.add(housenumber);
-//				contactData.add(country);
-//				contactData.add(birthday);
-//				
-//			return contactData;	
-//		}
-//	}
+//// End : Ultimative setter ////
 	
 }
