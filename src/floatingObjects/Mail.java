2a8bc5ddd3efafe8a273d251afc38123cfb6470e
Erik Heinisch <Imkerixus@gmail.com>
2014-01-30 13:01:49 +0100

    changed some variable names to achieve a more constant nameing

--- src/floatingObjects/Mail.java
+++ src/floatingObjects/Mail.java
@@ -6,11 +6,13 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;				
 import java.util.Date;
 
+import com.sun.org.apache.regexp.internal.recompile;
+
 @SuppressWarnings("serial")
 public class Mail implements Serializable
 {
 
-	private String subject,content,senderEmail,recipientEmail,cc,bcc;
+	private String subject,content,senderEmail,recipientEmail,cc,bcc,folder;
 	private Date date = new Date();
 	private int mail_id;
 	private ArrayList<String> attachements = null;
@@ -20,93 +22,69 @@ public class Mail implements Serializable
 		mail_id = p_mail_id;
 	}
 	
-////Begin : Ultimative getter ////
-	public Object get(String p_ArgumentToGet)
+	public void addAttachments(String p_allAtachments)
 	{
-		switch (p_ArgumentToGet) 
+		String[] splitedAttechments = p_allAtachments.split(" ");
+		for(String string : splitedAttechments)
 		{
-			case "subject": return subject;	
-			case "content": return content;
-			case "senderemail": return senderEmail;
-			case "recipientemail": return recipientEmail;
-			case "cc": return cc;
-			case "bcc": return bcc;
-			case "date": return date.toString();
-			case "mailid": return Integer.toString(mail_id);
-			case "attachements": return attachements;
-		
-			default: return "Mail oder Argument fehlerhaft";	
+			attachements.add(string);
 		}
 	}
-//// Begin : Ultimative getter ////	
-	
-////Begin : Ultimative setter ////
-	public Object set(String[] args)
+	public void replaceAttachments(String p_allAtachments)
 	{
-		switch (args[3]) 
-		{
-			case "subject": 
-				subject = args[4]; 
-			return args[3]+"="+args[4];	
-			
-			case "content": 
-				String temp_content = "";
-				for(int i = 4; i<args.length; i++)
-				{
-					temp_content+=args[i]+" ";
-				}
-				content = temp_content;
-			return args[3]+"="+args[4];
-			
-			case "senderemail": 
-				senderEmail = args[4];
-			return args[3]+"="+args[4];
-			
-			case "recipientemail": 
-				recipientEmail = args[4];
-			return args[3]+"="+args[4];
-			
-			case "cc": 
-				cc = args[4];
-			return args[3]+"="+args[4];
-			
-			case "bcc": 
-				bcc = args[4];
-			return args[3]+"="+args[4];
-			
-			case "date":
-				try 
-				{
-					SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
-					date = formatter.parse(args[4]);
-				} catch (ParseException e) {
-					e.printStackTrace();
-				}
-			return args[3]+"="+args[4];
-			
-			case "mail_id": 
-				mail_id = Integer.parseInt(args[4]);
-			return args[3]+"="+args[4];
-			
-			case "attachements_add":
-				for (String arg : args) 
-				{
-					attachements.add(arg);
-				}
-			return args[3]+"="+"all Added";
-			
-			case "attachements_replace":
-				attachements.clear();
-				for (String arg : args) 
-				{
-					attachements.add(arg);
-				}
-			return args[3]+"="+"all Added";
-		
-			default: return "Mail oder Argument fehlerhaft";	
-		}
+		attachements.clear();
+		addAttachments(p_allAtachments);
 	}
-//// Begin : Ultimative setter ////	
 	
-	
+//// Begin : Ultimative getter ////
+    public Object get(String p_ArgumentToGet)
+    {
+        switch (p_ArgumentToGet)
+        {
+            case "subject": return subject;        
+            case "content": return content;
+            case "senderemail": return senderEmail;
+            case "recipientemail": return recipientEmail;
+            case "cc": return cc;
+            case "bcc": return bcc;
+            case "date": return date.toString();
+            case "mail_id": return mail_id;
+            case "attachements": return attachements;
+            case "folder": return folder;
+    
+            default: return "Mail oder Argument fehlerhaft";        
+        }
+    }
+//// End : Ultimative getter ////        
+//// Begin : Ultimative setter ////
+    public void set(String p_setting, String[] p_value)
+    {
+        switch (p_setting)
+        {
+            case "subject":subject = p_value[0]; break;
+            case "content":
+            		content = "";
+            		for(String s : p_value)
+            		{
+            			content += s+" ";
+            		} break;
+            case "senderemail":senderEmail = p_value[0]; break;
+            case "recipientemail":recipientEmail = p_value[0]; break;
+            case "cc":cc = p_value[0]; break;
+            case "bcc":bcc = p_value[0]; break;
+            case "date":
+                    try
+                    {
+                            SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+                            date = formatter.parse(p_value[0]);
+                    } catch (ParseException e) {
+                            e.printStackTrace();
+                    } break;
+            case "mail_id":mail_id = Integer.parseInt(p_value[0]); break;
+            case "attachements_add":addAttachments(p_value[0]); break;
+            case "attachements_replace":replaceAttachments(p_value[0]);break;
+            case "folder" :folder = p_value[0]; break;
+        }
+    }
+//// End : Ultimative setter ////
 }
