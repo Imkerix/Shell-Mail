bb45cda886e991e0c9f5d44069b82eca9948a1ad
Erik Heinisch <Imkerixus@gmail.com>
2014-01-30 12:58:19 +0100

    no real changes in here

--- src/logic/Main.java
+++ src/logic/Main.java
@@ -1,8 +1,10 @@
 package logic;
 
+import com.martiansoftware.jsap.JSAPException;
+
 public class Main 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws JSAPException
 	{
 		@SuppressWarnings("unused")
 		JavaLogic mylogic = new JavaLogic(args);
