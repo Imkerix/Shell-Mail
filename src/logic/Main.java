d710abacee2bd0b6ebf542afd5f5c1ef9331859b
Erik Heinisch <Imkerixus@gmail.com>
2014-04-05 16:57:48 +0200

    +JavaDoc was added.

--- src/logic/Main.java
+++ src/logic/Main.java
@@ -2,11 +2,22 @@ package logic;
 
 import com.martiansoftware.jsap.JSAPException;
 
+/**
+ * The class to start the {@link logic.Backbone}.
+ */
 public class Main 
 {
+    /**
+     * Creates a new {@link logic.Backbone}.
+     * @param args The arguments given to the JVM.
+     * @throws JSAPException Parsing problems by {@link com.martiansoftware.jsap.JSAPException}.
+     */
 	public static void main(String[] args) throws JSAPException
 	{
 		@SuppressWarnings("unused")
+        /**
+         * The {@link logic.Backbone} object.
+         */
         Backbone mylogic = new Backbone(args);
 	}
 }
